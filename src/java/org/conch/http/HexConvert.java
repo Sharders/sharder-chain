ba2e0b00482d00bbc844cf4c8d7035ff6ff666bc
vitty <vitty@sharder.org>
2018-01-09 13:17:28 +0800

    Service and base tool

---
+++ src/java/org/conch/http/HexConvert.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright © 2017 sharder.org.
+ * Copyright © 2014-2017 ichaoj.com.
+ *
+ * See the LICENSE.txt file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with ichaoj.com,
+ * no part of the COS software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE.txt file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+
+package org.conch.http;
+
+import org.conch.util.Convert;
+import org.conch.util.JSON;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONStreamAware;
+
+import javax.servlet.http.HttpServletRequest;
+
+public final class HexConvert extends APIServlet.APIRequestHandler {
+
+    static final HexConvert instance = new HexConvert();
+
+    private HexConvert() {
+        super(new APITag[] {APITag.UTILS}, "string");
+    }
+
+    @Override
+    protected JSONStreamAware processRequest(HttpServletRequest req) {
+        String string = Convert.emptyToNull(req.getParameter("string"));
+        if (string == null) {
+            return JSON.emptyJSON;
+        }
+        JSONObject response = new JSONObject();
+        try {
+            byte[] asHex = Convert.parseHexString(string);
+            if (asHex.length > 0) {
+                response.put("text", Convert.toString(asHex));
+            }
+        } catch (RuntimeException ignore) {}
+        try {
+            byte[] asText = Convert.toBytes(string);
+            response.put("binary", Convert.toHexString(asText));
+        } catch (RuntimeException ignore) {}
+        return response;
+    }
+
+    @Override
+    protected boolean allowRequiredBlockParameters() {
+        return false;
+    }
+
+    @Override
+    protected boolean requireBlockchain() {
+        return false;
+    }
+
+}
