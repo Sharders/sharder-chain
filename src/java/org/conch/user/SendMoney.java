8a02608099449c5061d4c74cce4e6a6a04cabefa
Aron.j <xy@ichaoj.com>
2018-01-10 18:41:35 +0800

    ss transfer

---
+++ src/java/org/conch/user/SendMoney.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright © 2017 sharder.org.
+ * Copyright © 2014-2017 ichaoj.com.
+ *
+ * See the LICENSE.txt file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with ichaoj.com,
+ * no part of the COS software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE.txt file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+
+package org.conch.user;
+
+import org.conch.Account;
+import org.conch.Attachment;
+import org.conch.Constants;
+import org.conch.Conch;
+import org.conch.ConchException;
+import org.conch.Transaction;
+import org.conch.util.Convert;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONStreamAware;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public final class SendMoney extends UserServlet.UserRequestHandler {
+
+    static final SendMoney instance = new SendMoney();
+
+    private SendMoney() {}
+
+    @Override
+    JSONStreamAware processRequest(HttpServletRequest req, User user) throws ConchException.ValidationException, IOException {
+        if (user.getSecretPhrase() == null) {
+            return null;
+        }
+
+        String recipientValue = req.getParameter("recipient");
+        String amountValue = req.getParameter("amountSS");
+        String feeValue = req.getParameter("feeSS");
+        String deadlineValue = req.getParameter("deadline");
+        String secretPhrase = req.getParameter("secretPhrase");
+
+        long recipient;
+        long amountNQT = 0;
+        long feeNQT = 0;
+        short deadline = 0;
+
+        try {
+
+            recipient = Convert.parseUnsignedLong(recipientValue);
+            if (recipient == 0) throw new IllegalArgumentException("invalid recipient");
+            amountNQT = Convert.parseSS(amountValue.trim());
+            feeNQT = Convert.parseSS(feeValue.trim());
+            deadline = (short)(Double.parseDouble(deadlineValue) * 60);
+
+        } catch (RuntimeException e) {
+
+            JSONObject response = new JSONObject();
+            response.put("response", "notifyOfIncorrectTransaction");
+            response.put("message", "One of the fields is filled incorrectly!");
+            response.put("recipient", recipientValue);
+            response.put("amountSS", amountValue);
+            response.put("feeSS", feeValue);
+            response.put("deadline", deadlineValue);
+
+            return response;
+        }
+
+        if (! user.getSecretPhrase().equals(secretPhrase)) {
+
+            JSONObject response = new JSONObject();
+            response.put("response", "notifyOfIncorrectTransaction");
+            response.put("message", "Wrong secret phrase!");
+            response.put("recipient", recipientValue);
+            response.put("amountSS", amountValue);
+            response.put("feeSS", feeValue);
+            response.put("deadline", deadlineValue);
+
+            return response;
+
+        } else if (amountNQT <= 0 || amountNQT > Constants.MAX_BALANCE_NQT) {
+
+            JSONObject response = new JSONObject();
+            response.put("response", "notifyOfIncorrectTransaction");
+            response.put("message", "\"Amount\" must be greater than 0!");
+            response.put("recipient", recipientValue);
+            response.put("amountSS", amountValue);
+            response.put("feeSS", feeValue);
+            response.put("deadline", deadlineValue);
+
+            return response;
+
+        } else if (feeNQT < Constants.ONE_SS || feeNQT > Constants.MAX_BALANCE_NQT) {
+
+            JSONObject response = new JSONObject();
+            response.put("response", "notifyOfIncorrectTransaction");
+            response.put("message", "\"Fee\" must be at least 1 SS!");
+            response.put("recipient", recipientValue);
+            response.put("amountSS", amountValue);
+            response.put("feeSS", feeValue);
+            response.put("deadline", deadlineValue);
+
+            return response;
+
+        } else if (deadline < 1 || deadline > 1440) {
+
+            JSONObject response = new JSONObject();
+            response.put("response", "notifyOfIncorrectTransaction");
+            response.put("message", "\"Deadline\" must be greater or equal to 1 minute and less than 24 hours!");
+            response.put("recipient", recipientValue);
+            response.put("amountSS", amountValue);
+            response.put("feeSS", feeValue);
+            response.put("deadline", deadlineValue);
+
+            return response;
+
+        }
+
+        Account account = Account.getAccount(user.getPublicKey());
+        if (account == null || Math.addExact(amountNQT, feeNQT) > account.getUnconfirmedBalanceNQT()) {
+
+            JSONObject response = new JSONObject();
+            response.put("response", "notifyOfIncorrectTransaction");
+            response.put("message", "Not enough funds!");
+            response.put("recipient", recipientValue);
+            response.put("amountSS", amountValue);
+            response.put("feeSS", feeValue);
+            response.put("deadline", deadlineValue);
+
+            return response;
+
+        } else {
+
+            final Transaction transaction = Conch.newTransactionBuilder(user.getPublicKey(),
+                    amountNQT, feeNQT, deadline, Attachment.ORDINARY_PAYMENT).recipientId(recipient).build(secretPhrase);
+
+            Conch.getTransactionProcessor().broadcast(transaction);
+
+            return JSONResponses.NOTIFY_OF_ACCEPTED_TRANSACTION;
+
+        }
+    }
+
+    @Override
+    boolean requirePost() {
+        return true;
+    }
+
+}
