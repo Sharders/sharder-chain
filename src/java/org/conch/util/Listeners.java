86530fc1a6a9466b88975966433b5697099b9b43
ben <xy200362@163.com>
2018-01-10 21:57:11 +0800

    listener

---
+++ src/java/org/conch/util/Listeners.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright © 2017 sharder.org.
+ * Copyright © 2014-2017 ichaoj.com.
+ *
+ * See the LICENSE.txt file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with ichaoj.com,
+ * no part of the COS software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE.txt file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+
+package org.conch.util;
+
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public final class Listeners<T,E extends Enum<E>> {
+
+    private final ConcurrentHashMap<Enum<E>, List<Listener<T>>> listenersMap = new ConcurrentHashMap<>();
+
+    public boolean addListener(Listener<T> listener, Enum<E> eventType) {
+        synchronized (eventType) {
+            List<Listener<T>> listeners = listenersMap.get(eventType);
+            if (listeners == null) {
+                listeners = new CopyOnWriteArrayList<>();
+                listenersMap.put(eventType, listeners);
+            }
+            return listeners.add(listener);
+        }
+    }
+
+    public boolean removeListener(Listener<T> listener, Enum<E> eventType) {
+        synchronized (eventType) {
+            List<Listener<T>> listeners = listenersMap.get(eventType);
+            if (listeners != null) {
+                return listeners.remove(listener);
+            }
+        }
+        return false;
+    }
+
+    public void notify(T t, Enum<E> eventType) {
+        List<Listener<T>> listeners = listenersMap.get(eventType);
+        if (listeners == null)  return;
+
+        for (Listener<T> listener : listeners)  listener.notify(t);
+    }
+
+}
---
+++ src/java/org/conch/util/Observable.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright © 2017 sharder.org.
+ * Copyright © 2014-2017 ichaoj.com.
+ *
+ * See the LICENSE.txt file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with ichaoj.com,
+ * no part of the COS software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE.txt file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+
+package org.conch.util;
+
+public interface Observable<T,E extends Enum<E>> {
+
+    boolean addListener(Listener<T> listener, E eventType);
+
+    boolean removeListener(Listener<T> listener, E eventType);
+
+}
