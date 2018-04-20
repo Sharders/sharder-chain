fa43716af16e2bee61d1dc723d22232fee756c1c
wj507 <wuji507@gmail.com>
2018-04-20 16:49:11 +0800

    change the test network port

--- src/java/org/conch/http/API.java
+++ src/java/org/conch/http/API.java
@@ -58,8 +58,8 @@ import static org.conch.http.JSONResponses.*;
 
 public final class API {
 
-    public static final int TESTNET_API_PORT = 6876;
-    public static final int TESTNET_API_SSLPORT = 6877;
+    public static final int TESTNET_API_PORT = 8215;
+    public static final int TESTNET_API_SSLPORT = 8218;
     private static final String[] DISABLED_HTTP_METHODS = {"TRACE", "OPTIONS", "HEAD"};
 
     public static final int openAPIPort;
