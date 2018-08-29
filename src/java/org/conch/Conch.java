a3760c69782afe1468b96b5027d5a6ec56e6e866
bubai <jw@sharder.org>
2018-08-29 15:36:16 +0800

    set version from 0.0.8 to 0.0.9

--- src/java/org/conch/Conch.java
+++ src/java/org/conch/Conch.java
@@ -57,7 +57,7 @@ import java.util.Properties;
 
 public final class Conch {
 
-    public static final String VERSION = "0.0.8";
+    public static final String VERSION = "0.0.9";
     public static final String APPLICATION = "COS";
 
     private static volatile Time time = new Time.EpochTime();
