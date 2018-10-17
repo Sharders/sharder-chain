c8814869458f8943521df225983949f5b9f4e454
jiangbubai <jiangbubai@gmail.com>
2018-10-17 14:19:59 +0800

    [enhancement] handle the exception when channel is not RS format address

--- src/java/org/conch/http/biz/handler/GetBizBlocks.java
+++ src/java/org/conch/http/biz/handler/GetBizBlocks.java
@@ -28,6 +28,7 @@ import org.conch.Conch;
 import org.conch.ConchException;
 import org.conch.db.DbIterator;
 import org.conch.http.*;
+import org.conch.util.Logger;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONStreamAware;
 
@@ -73,7 +74,7 @@ public final class GetBizBlocks extends APIServlet.APIRequestHandler {
             ArrayList list = mapper.readValue(dtrJson, new TypeReference<List<Map<String, Object>>>(){});
             response.addAll(list);
         } catch (IOException e) {
-            e.printStackTrace();
+            Logger.logErrorMessage(e.getMessage());
             return JSONResponses.BIZ_JSON_IO_ERROR;
         }
 
