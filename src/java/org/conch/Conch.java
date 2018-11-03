46efa7ac6092857f4f4514d5cd3cf4a531672361
Ben <xy@sharder.org>
2018-11-03 20:17:15 +0800

    change the format

--- src/java/org/conch/Conch.java
+++ src/java/org/conch/Conch.java
@@ -49,8 +49,8 @@ import java.util.*;
 
 public final class Conch {
 
-    public static final String VERSION = "0.1.0";
-    public static final String STAGE = "-Beta";
+    public static final String VERSION="0.1.0";
+    public static final String STAGE="-Beta";
     public static final String APPLICATION = "COS";
 
     private static volatile Time time = new Time.EpochTime();
