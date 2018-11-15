c647b54676a8444408b792249a227daf642bdbb8
sorayu <sorayu@gmail.com>
2018-11-15 10:56:04 +0800

    漏提的login页面

---
+++ ui/src/views/login/index.vue
@@ -0,0 +1,30 @@
+<template>
+
+    <div>
+        this is login page
+
+        <button @click="login()"></button>
+    </div>
+
+</template>
+
+<script>
+    import store from "../../store"
+    export default {
+        name: "index",
+        data () {
+            return {
+            }
+        },
+        methods: {
+            login: function () {
+                store.commit("loginState")
+                console.log(store.state.isLogin)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
