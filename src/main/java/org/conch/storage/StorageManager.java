017a2e59ba1d4711750024ce4af559e08931298d
ben-xy <xy200362@163.com>
2018-11-27 16:10:50 +0800

    missing storage files

---
+++ src/main/java/org/conch/storage/StorageManager.java
@@ -0,0 +1,35 @@
+/*
+ *  Copyright © 2017-2018 Sharder Foundation.
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  version 2 as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, you can visit it at:
+ *  https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ *
+ *  This software uses third party libraries and open-source programs,
+ *  distributed under licenses described in 3RD-PARTY-LICENSES.
+ *
+ */
+
+package org.conch.storage;
+
+import org.conch.Conch;
+import org.conch.storage.ipfs.IpfsService;
+
+public class StorageManager {
+    public static void init() {
+
+        if (!Conch.getBooleanProperty("sharder.enableStorage")) return;
+
+        //init and run daemon
+        IpfsService.init();
+    }
+}
