03435c5d0cb9d155f25f9df13b883f6938441031
ben-xy <xy200362@163.com>
2018-12-26 10:45:07 +0800

    update dev port

--- ui/src/httpConfig/api.js
+++ ui/src/httpConfig/api.js
@@ -1,8 +1,9 @@
-const SERVER_API = "https://test.sharder.io";
+const SERVER_API = "http://localhost:3215";
 // const SERVER_API = "https://biz.sharder.io";
 // const SERVER_API_DEV = "47.107.188.3:8215";
 // const SERVER_API_DEV = "http://localhost:8215";
-const SERVER_API_DEV = "http://192.168.31.115:8215";
+const SERVER_API_TEST = "http://localhost:8215";
+const SERVER_API_DEV = "http://localhost:9215";
 
 
 function getUrl() {
