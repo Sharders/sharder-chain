520ae450ce4ce6ff0c8b9cd18ce75bd7634e00e9
cloudsen <www.402645063@gmail.com>
2019-02-18 20:03:13 +0800

    doc(constants): add constants message

--- src/main/java/org/conch/common/Constants.java
+++ src/main/java/org/conch/common/Constants.java
@@ -348,6 +348,10 @@ public final class Constants {
 
     public static final String SUCCESS = "success";
 
+    public static final String DATA = "data";
+
+    public static final String STATUS = "status";
+
     public static final String HTTP = "http://";
 
     public static final String HTTPS = "https://";
