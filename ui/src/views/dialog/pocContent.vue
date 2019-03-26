7db08160cbe6220c318a04d18df409410059a875
ben-xy <xy200362@163.com>
2019-03-26 17:26:03 +0800

    >mutiple-languages of PoC weight table

--- ui/src/views/dialog/pocContent.vue
+++ ui/src/views/dialog/pocContent.vue
@@ -3,95 +3,94 @@
         <div class="template">
             <p v-if="pocInfo.weightMap">
                 <strong>{{$t('poc.weight')}}</strong>
-                <span>{{$t('poc.nodeType')}}: {{pocInfo.weightMap.node}}</span>
-                <span>开启服务: {{pocInfo.weightMap.serverOpen}}</span>
-                <span>SS持有量: {{pocInfo.weightMap.ssHold}}</span>
-                <span>硬件配置: {{pocInfo.weightMap.hardwareConfig}}</span>
-                <span>硬件配置: {{pocInfo.weightMap.hardwareConfig}}</span>
-                <span>网络配置: {{pocInfo.weightMap.networkConfig}}</span>
-                <span>交易处理性能:{{pocInfo.weightMap.txPerformance}}</span>
+                <span>{{$t('poc.node_type')}}: {{pocInfo.weightMap.node}}%</span>
+                <span>{{$t('poc.weight_role')}}: {{pocInfo.weightMap.serverOpen}}%</span>
+                <span>{{$t('poc.weight_ss_held')}}: {{pocInfo.weightMap.ssHold}}%</span>
+                <span>{{$t('poc.weight_hardware_config')}}: {{pocInfo.weightMap.hardwareConfig}}%</span>
+                <span>{{$t('poc.weight_network_config')}}: {{pocInfo.weightMap.networkConfig}}%</span>
+                <span>{{$t('poc.weight_tx_performance')}}:{{pocInfo.weightMap.txPerformance}}%</span>
             </p>
             <p v-if="pocInfo.nodeTypeTemplate">
-                <strong>节点类型分数</strong>
-                <span>基金会节点: {{pocInfo.nodeTypeTemplate["1"]}}</span>
-                <span>社区认证节点: {{pocInfo.nodeTypeTemplate["2"]}}</span>
-                <span>HUB节点: {{pocInfo.nodeTypeTemplate["4"]}}</span>
-                <span>BOX节点: {{pocInfo.nodeTypeTemplate["5"]}}</span>
-                <span>普通节点: {{pocInfo.nodeTypeTemplate["3"]}}</span>
+                <strong>{{$t('poc.node_type_score')}}</strong>
+                <span>{{$t('poc.sharder_node')}}: {{pocInfo.nodeTypeTemplate["1"]}}</span>
+                <span>{{$t('poc.community_node')}}: {{pocInfo.nodeTypeTemplate["2"]}}</span>
+                <span>{{$t('poc.hub_node')}}: {{pocInfo.nodeTypeTemplate["4"]}}</span>
+                <span>{{$t('poc.box_node')}}: {{pocInfo.nodeTypeTemplate["5"]}}</span>
+                <span>{{$t('poc.normal_node')}}: {{pocInfo.nodeTypeTemplate["3"]}}</span>
             </p>
             <p v-if="pocInfo.serverOpenTemplate">
-                <strong>开启服务分数</strong>
-                <span>矿工: {{pocInfo.serverOpenTemplate["32"]}}</span>
-                <span>观察者: {{pocInfo.serverOpenTemplate["64"]}}</span>
-                <span>穿透者: {{pocInfo.serverOpenTemplate["128"]}}</span>
-                <span>存储者: {{pocInfo.serverOpenTemplate["256"]}}</span>
-                <span>证明者: {{pocInfo.serverOpenTemplate["512"]}}</span>
+                <strong>{{$t('poc.role_score')}}</strong>
+                <span>{{$t('poc.role_miner')}}: {{pocInfo.serverOpenTemplate["32"]}}</span>
+                <span>{{$t('poc.role_watcher')}}: {{pocInfo.serverOpenTemplate["64"]}}</span>
+                <span>{{$t('poc.role_traversal')}}: {{pocInfo.serverOpenTemplate["128"]}}</span>
+                <span>{{$t('poc.role_storer')}}: {{pocInfo.serverOpenTemplate["256"]}}</span>
+                <span>{{$t('poc.role_prover')}}: {{pocInfo.serverOpenTemplate["512"]}}</span>
             </p>
             <p v-if="pocInfo.txPerformanceTemplate">
-                <strong>交易处理性能分数</strong>
-                <span>低: {{pocInfo.txPerformanceTemplate["1"]}}</span>
-                <span>中: {{pocInfo.txPerformanceTemplate["2"]}}</span>
-                <span>高: {{pocInfo.txPerformanceTemplate["3"]}}</span>
+                <strong>{{$t('poc.tx_performance_score')}}</strong>
+                <span>{{$t('poc.item_low')}}: {{pocInfo.txPerformanceTemplate["1"]}}</span>
+                <span>{{$t('poc.item_med')}}: {{pocInfo.txPerformanceTemplate["2"]}}</span>
+                <span>{{$t('poc.item_high')}}: {{pocInfo.txPerformanceTemplate["3"]}}</span>
             </p>
             <p v-if="pocInfo.hardwareConfigTemplate">
-                <strong>硬件分数</strong>
-                <span>低: {{pocInfo.hardwareConfigTemplate["1"]}}</span>
-                <span>中: {{pocInfo.hardwareConfigTemplate["2"]}}</span>
-                <span>高: {{pocInfo.hardwareConfigTemplate["3"]}}</span>
+                <strong>{{$t('poc.hardware_score')}}</strong>
+                <span>{{$t('poc.item_low')}}: {{pocInfo.hardwareConfigTemplate["1"]}}</span>
+                <span>{{$t('poc.item_med')}}: {{pocInfo.hardwareConfigTemplate["2"]}}</span>
+                <span>{{$t('poc.item_high')}}: {{pocInfo.hardwareConfigTemplate["3"]}}</span>
             </p>
             <p v-if="pocInfo.networkConfigTemplate">
-                <strong>网络分数</strong>
-                <span>极差: {{pocInfo.networkConfigTemplate["0"]}}</span>
-                <span>低: {{pocInfo.networkConfigTemplate["1"]}}</span>
-                <span>中: {{pocInfo.networkConfigTemplate["2"]}}</span>
-                <span>高: {{pocInfo.networkConfigTemplate["3"]}}</span>
+                <strong>{{$t('poc.network_score')}}</strong>
+                <span>{{$t('poc.item_very_low')}}: {{pocInfo.networkConfigTemplate["0"]}}</span>
+                <span>{{$t('poc.item_low')}}: {{pocInfo.networkConfigTemplate["1"]}}</span>
+                <span>{{$t('poc.item_med')}}: {{pocInfo.networkConfigTemplate["2"]}}</span>
+                <span>{{$t('poc.item_high')}}: {{pocInfo.networkConfigTemplate["3"]}}</span>
             </p>
             <p v-if="pocInfo.generationMissingTemplate">
-                <strong>出块错过数惩罚表(扣分)</strong>
-                <span>低: {{pocInfo.generationMissingTemplate["1"]}}</span>
-                <span>中: {{pocInfo.generationMissingTemplate["2"]}}</span>
-                <span>高: {{pocInfo.generationMissingTemplate["3"]}}</span>
+                <strong>{{$t('poc.amount_of_loss_blocks')}}</strong>
+                <span>{{$t('poc.item_low')}}: {{pocInfo.generationMissingTemplate["1"]}}</span>
+                <span>{{$t('poc.item_med')}}: {{pocInfo.generationMissingTemplate["2"]}}</span>
+                <span>{{$t('poc.item_high')}}: {{pocInfo.generationMissingTemplate["3"]}}</span>
             </p>
             <p v-if="pocInfo.bocSpeedTemplate">
-                <strong>分叉收敛惩罚表(扣分)</strong>
-                <span>硬分叉: {{pocInfo.bocSpeedTemplate["1"]}}</span>
-                <span>慢: {{pocInfo.bocSpeedTemplate["2"]}}</span>
-                <span>中: {{pocInfo.bocSpeedTemplate["0"]}}</span>
+                <strong>{{$t('poc.fork_convergence_speed')}}</strong>
+                <span>{{$t('poc.hard_fork')}}: {{pocInfo.bocSpeedTemplate["1"]}}</span>
+                <span>{{$t('poc.item_slow')}}: {{pocInfo.bocSpeedTemplate["2"]}}</span>
+                <span>{{$t('poc.item_med')}}: {{pocInfo.bocSpeedTemplate["0"]}}</span>
             </p>
         </div>
         <div class="online-rate" v-if="pocInfo.onlineRateTemplate">
             <div>
-                <h3>基金会节点</h3>
-                <p>99.00%&lt;在线率&lt;99.99%<span>{{pocInfo.onlineRateTemplate.FOUNDATION["0"]}}</span></p>
-                <p>97.00%&lt;在线率&lt;99.00%<span>{{pocInfo.onlineRateTemplate.FOUNDATION["1"]}}</span></p>
-                <p>在线率&lt;97.00% <span>{{pocInfo.onlineRateTemplate.FOUNDATION["3"]}}</span></p>
+                <h3>{{$t('poc.sharder_node')}}</h3>
+                <p>99.00%&lt;{{$t('poc.uptime')}}&lt;99.99%<span>{{pocInfo.onlineRateTemplate.FOUNDATION["0"]}}</span></p>
+                <p>97.00%&lt;{{$t('poc.uptime')}}&lt;99.00%<span>{{pocInfo.onlineRateTemplate.FOUNDATION["1"]}}</span></p>
+                <p>{{$t('poc.uptime')}}&lt;97.00% <span>{{pocInfo.onlineRateTemplate.FOUNDATION["3"]}}</span></p>
             </div>
             <div>
-                <h3>社区认证节点</h3>
-                <p>97.00%&lt;在线率&lt;99.00%<span>{{pocInfo.onlineRateTemplate.COMMUNITY["1"]}}</span></p>
-                <p>90.00%&lt;在线率&lt;97.00%<span>{{pocInfo.onlineRateTemplate.COMMUNITY["2"]}}</span></p>
-                <p>在线率&lt;90.00%<span>{{pocInfo.onlineRateTemplate.COMMUNITY["4"]}}</span></p>
+                <h3>{{$t('poc.community_node')}}</h3>
+                <p>97.00%&lt;{{$t('poc.uptime')}}&lt;99.00%<span>{{pocInfo.onlineRateTemplate.COMMUNITY["1"]}}</span></p>
+                <p>90.00%&lt;{{$t('poc.uptime')}}&lt;97.00%<span>{{pocInfo.onlineRateTemplate.COMMUNITY["2"]}}</span></p>
+                <p>{{$t('poc.uptime')}}&lt;90.00%<span>{{pocInfo.onlineRateTemplate.COMMUNITY["4"]}}</span></p>
             </div>
             <div>
-                <h3>Hub节点</h3>
-                <p>在线率&lt;99.00%<span>{{pocInfo.onlineRateTemplate.HUB["5"]}}</span></p>
-                <p>在线率&lt;97.00%<span>{{pocInfo.onlineRateTemplate.HUB["6"]}}</span></p>
-                <p>在线率&lt;90.00%<span>{{pocInfo.onlineRateTemplate.HUB["4"]}}</span></p>
+                <h3>{{$t('poc.hub_node')}}</h3>
+                <p>{{$t('poc.uptime')}}&lt;99.00%<span>{{pocInfo.onlineRateTemplate.HUB["5"]}}</span></p>
+                <p>{{$t('poc.uptime')}}&lt;97.00%<span>{{pocInfo.onlineRateTemplate.HUB["6"]}}</span></p>
+                <p>{{$t('poc.uptime')}}&lt;90.00%<span>{{pocInfo.onlineRateTemplate.HUB["4"]}}</span></p>
             </div>
             <div>
-                <h3>Box节点</h3>
-                <p>在线率&lt;99.00%<span>{{pocInfo.onlineRateTemplate.BOX["5"]}}</span></p>
-                <p>在线率&lt;97.00%<span>{{pocInfo.onlineRateTemplate.BOX["6"]}}</span></p>
-                <p>在线率&lt;90.00%<span>{{pocInfo.onlineRateTemplate.BOX["4"]}}</span></p>
+                <h3>{{$t('poc.box_node')}}</h3>
+                <p>{{$t('poc.uptime')}}&lt;99.00%<span>{{pocInfo.onlineRateTemplate.BOX["5"]}}</span></p>
+                <p>{{$t('poc.uptime')}}&lt;97.00%<span>{{pocInfo.onlineRateTemplate.BOX["6"]}}</span></p>
+                <p>{{$t('poc.uptime')}}&lt;90.00%<span>{{pocInfo.onlineRateTemplate.BOX["4"]}}</span></p>
             </div>
             <div>
-                <h3>普通节点</h3>
-                <p>在线率&lt;97.00%<span>{{pocInfo.onlineRateTemplate.NORMAL["6"]}}</span></p>
-                <p>在线率&lt;90.00%<span>{{pocInfo.onlineRateTemplate.NORMAL["7"]}}</span></p>
+                <h3>{{$t('poc.normal_node')}}</h3>
+                <p>{{$t('poc.uptime')}}&lt;97.00%<span>{{pocInfo.onlineRateTemplate.NORMAL["6"]}}</span></p>
+                <p>{{$t('poc.uptime')}}&lt;90.00%<span>{{pocInfo.onlineRateTemplate.NORMAL["7"]}}</span></p>
             </div>
         </div>
 
-        <p class="template-version">版本号: {{pocInfo.templateVersion}}</p>
+        <p class="template-version">{{$t('poc.template_version')}} {{pocInfo.templateVersion}}</p>
         <p class="testnet-tips">{{$t('poc.block_reward_tips_phase1')}}</p>
     </div>
 </template>
