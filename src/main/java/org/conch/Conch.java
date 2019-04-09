68b8994f5aa570f42e8da6f7a2c931546ed8e0c2
ben-xy <xy200362@163.com>
2019-04-09 16:53:19 +0800

    >rollback cos version

--- src/main/java/org/conch/Conch.java
+++ src/main/java/org/conch/Conch.java
@@ -85,7 +85,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public final class Conch {
 
-    public static final String VERSION = "0.1.3";
+    public static final String VERSION = "0.1.2";
     public static final String STAGE = "-Alpha";
     public static final String APPLICATION = "COS";
 
