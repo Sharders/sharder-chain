be8a215c071c0d2853b59fd8d24a37af2e027ed0
ben-xy <xy200362@163.com>
2019-06-05 17:15:55 +0800

    >pool unique validation

--- src/main/java/org/conch/http/GetPeers.java
+++ src/main/java/org/conch/http/GetPeers.java
@@ -21,10 +21,10 @@
 
 package org.conch.http;
 
+import org.conch.consensus.poc.fork.ForkConvergeTest;
 import org.conch.peer.Peer;
 import org.conch.peer.Peers;
 import org.conch.util.Convert;
-import org.conch.peer.Peers;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONStreamAware;
@@ -92,9 +92,10 @@ public final class GetPeers extends APIServlet.APIRequestHandler {
                 peers.forEach(peer -> peersJSON.add(peer.getHost()));
             }
         }
-
+        ForkConvergeTest.printPeerClient();
         JSONObject response = new JSONObject();
         response.put("peers", peersJSON);
+       
         return response;
     }
 
@@ -102,5 +103,4 @@ public final class GetPeers extends APIServlet.APIRequestHandler {
     protected boolean allowRequiredBlockParameters() {
         return false;
     }
-
 }
