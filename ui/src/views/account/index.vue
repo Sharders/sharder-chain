e67f104945b2345441695a9f129ec8d85c1c9d9b
cj-ldn <ldn66666@163.com>
2020-03-04 18:19:01 +0800

    账户信息添加提示   优化地图显示

--- ui/src/views/account/index.vue
+++ ui/src/views/account/index.vue
@@ -659,11 +659,21 @@
                         </td>
                     </tr>
                     <tr>
-                        <th>{{$t('account_info.account_balance')}}</th>
+                        <th>
+                            {{$t('account_info.account_balance')}}
+                            <el-tooltip class="item" effect="dark" :content="$t('account_info.account_balance_explain')" placement="top-start">
+                                <p class="el-icon-info"></p>
+                            </el-tooltip>
+                        </th>
                         <td>{{$global.getSSNumberFormat(accountInfo.balanceNQT)}}</td>
                     </tr>
                     <tr>
-                        <th>{{$t('account_info.account_available_balance')}}</th>
+                        <th>
+                            {{$t('account_info.account_available_balance')}}
+                            <el-tooltip class="item" effect="dark" :content="$t('account_info.account_name_not_set_explain')" placement="top-start">
+                                <p class="el-icon-info"></p>
+                            </el-tooltip>
+                        </th>
                         <td>{{$global.getSSNumberFormat(accountInfo.effectiveBalanceNQT)}}</td>
                     </tr>
                     <tr>
