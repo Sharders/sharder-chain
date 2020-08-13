1ed7c3debdb4989737ba059dce91733c153afd90
ben-xy <xy20036@163.com>
2020-08-13 21:39:47 +0800

    add certified peer table implementation; get last height method of poc score table

--- src/main/java/org/conch/consensus/poc/db/PocDb.java
+++ src/main/java/org/conch/consensus/poc/db/PocDb.java
@@ -22,9 +22,186 @@ import java.util.Map;
  * @since 2019-05-17
  */
 public class PocDb  {
+
+    /**
+     * poc score table
+     */
     private static class PocScoreTable extends DerivedDbTable {
         public PocScoreTable() {
             super("account_poc_score");
+        }
+
+        public int countAndRollback(int height) {
+            if (!Db.db.isInTransaction()) {
+                throw new IllegalStateException("Not in transaction");
+            }
+            try (Connection con = Db.db.getConnection();
+                 PreparedStatement pstmtDelete = con.prepareStatement("DELETE FROM ACCOUNT_POC_SCORE WHERE height > ?")) {
+                pstmtDelete.setInt(1, height);
+                return pstmtDelete.executeUpdate();
+            } catch (SQLException e) {
+                throw new RuntimeException(e.toString(), e);
+            }
+        }
+
+        public Map<Long,PocScore> listAll() {
+            Map<Long,PocScore> scoreMap = Maps.newHashMap();
+            Connection con = null;
+            try {
+                con = Db.db.getConnection();
+                PreparedStatement pstmt = con.prepareStatement("SELECT poc_detail AS detail FROM account_poc_score ORDER BY height DESC");
+
+                ResultSet rs = pstmt.executeQuery();
+                while(rs.next()){
+                    try{
+                        PocScore pocScore = new PocScore(rs.getString("detail"));
+
+                        // compare the height
+                        if(scoreMap.containsKey(pocScore.getAccountId())){
+                            PocScore oldScore = scoreMap.get(pocScore.getAccountId());
+                            if(oldScore.getHeight() < pocScore.getHeight()){
+                                scoreMap.put(pocScore.getAccountId(), pocScore);
+                            }
+                        }else{
+                            scoreMap.put(pocScore.getAccountId(), pocScore);
+                        }
+                    }catch (Exception e) {
+                        // continue to fetch next
+                        System.err.println(e);
+                    }
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e.toString(), e);
+            }finally {
+                DbUtils.close(con);
+            }
+            return scoreMap;
+        }
+
+        public String get(long accountId, int height, boolean loadHistory) {
+            if(height < 0) return null;
+
+            // close the start height in query
+            int appointStart = -1;
+
+            Connection con = null;
+            try {
+                con = Db.db.getConnection();
+                PreparedStatement pstmt = con.prepareStatement("SELECT poc_detail AS detail "
+                        + "FROM account_poc_score WHERE account_id = ?"
+                        + (loadHistory ? " AND height <= ?" : " AND height = ?")
+                        + (appointStart != -1 ? " AND height > ?" : "")
+                        + " ORDER BY height DESC LIMIT 1");
+
+                pstmt.setLong(1, accountId);
+                pstmt.setInt(2, height);
+
+                if(appointStart != -1) {
+                    pstmt.setInt(3, appointStart);
+                }
+
+                ResultSet rs = pstmt.executeQuery();
+                if(rs !=null && rs.next()){
+                    return rs.getString("detail");
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e.toString(), e);
+            }finally {
+                DbUtils.close(con);
+            }
+            return null;
+        }
+
+        public void saveOrUpdate(Connection con, PocScore pocScore) throws SQLException {
+            PreparedStatement pstmtCount = con.prepareStatement("SELECT db_id from account_poc_score "
+                    + "WHERE account_id = ? AND height = ?");
+            pstmtCount.setLong(1, pocScore.getAccountId());
+            pstmtCount.setInt(2, pocScore.getHeight());
+
+            ResultSet rs = pstmtCount.executeQuery();
+            boolean exist = (rs != null) && rs.next();
+
+            if(exist){
+                update(con, pocScore);
+            }else{
+                insert(con, pocScore);
+            }
+        }
+
+        public int insert(Connection con, PocScore pocScore) throws SQLException {
+            if(con == null) return 0;
+
+            PreparedStatement pstmtInsert = con.prepareStatement("INSERT INTO account_poc_score(account_id, "
+                    + " poc_score, height, poc_detail) VALUES(?, ?, ?, ?)");
+
+            pstmtInsert.setLong(1, pocScore.getAccountId());
+            pstmtInsert.setLong(2, pocScore.total().longValue());
+            pstmtInsert.setInt(3, pocScore.getHeight());
+            pstmtInsert.setString(4, pocScore.toSimpleJson());
+            return pstmtInsert.executeUpdate();
+        }
+
+        public int update(Connection con, PocScore pocScore) throws SQLException {
+            String detail = pocScore.toSimpleJson();
+            if(con == null || StringUtils.isEmpty(detail) || pocScore.getAccountId() == -1 || pocScore.getHeight() < 0 ){
+                return 0;
+            }
+
+            PreparedStatement pstmtUpdate = con.prepareStatement("UPDATE account_poc_score SET poc_score=?, poc_detail=? WHERE account_id = ? AND height = ?");
+
+            pstmtUpdate.setLong(1, pocScore.total().longValue());
+            pstmtUpdate.setString(2, detail);
+            pstmtUpdate.setLong(3, pocScore.getAccountId());
+            pstmtUpdate.setInt(4, pocScore.getHeight());
+            return pstmtUpdate.executeUpdate();
+        }
+
+        public void delete(long accountId, int height) {
+            Connection con = null;
+            try {
+                con = Db.db.getConnection();
+                PreparedStatement pstmtDelete = con.prepareStatement("DELETE FROM account_poc_score WHERE account_id = ? AND height = ?");
+
+                pstmtDelete.setLong(1, accountId);
+                pstmtDelete.setInt(2, height);
+                pstmtDelete.executeUpdate();
+            } catch (SQLException e) {
+                throw new RuntimeException(e.toString(), e);
+            } finally {
+                DbUtils.close(con);
+            }
+        }
+
+        @Override
+        public void rollback(int height) {
+            countAndRollback(height);
+        }
+
+        /**
+         * Trim the poc score table
+         *
+         * @param   height                  Trim height
+         */
+        @Override
+        public void trim(int height) {
+            try (Connection con = db.getConnection();
+                 PreparedStatement pstmt = con.prepareStatement("DELETE FROM account_poc_score WHERE height <= ?")) {
+                pstmt.setInt(1, height);
+                pstmt.executeUpdate();
+            } catch (SQLException e) {
+                throw new RuntimeException(e.toString(), e);
+            }
+        }
+    }
+    private static final PocScoreTable pocScoreTable = new PocScoreTable();
+
+
+    /**
+     * certified peer table
+     */
+    private static class CertifiedPeerTable extends DerivedDbTable {
+        public CertifiedPeerTable() {
+            super("certified_peer");
         }
 
         public int countAndRollback(int height) {
@@ -199,8 +376,27 @@ public class PocDb  {
             }
         }
     }
-    private static final PocScoreTable pocScoreTable = new PocScoreTable();
+    private static final CertifiedPeerTable certifiedPeerTable = new CertifiedPeerTable();
 
+    public static int getLastHeight(){
+        int lastHeight = -1;
+        Connection con = null;
+        try {
+            con = Db.db.getConnection();
+            PreparedStatement pstmt = con.prepareStatement("SELECT height FROM account_poc_score"
+                    + " ORDER BY height DESC LIMIT 1");
+
+            ResultSet rs = pstmt.executeQuery();
+            if(rs !=null && rs.next()){
+                lastHeight = rs.getInt("height");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e.toString(), e);
+        }finally {
+            DbUtils.close(con);
+        }
+        return lastHeight;
+    }
 
     public static void batchUpdate(Connection con, List<PocScore> pocScoreList) {
         if (pocScoreList == null || pocScoreList.size() < 0 ) {
