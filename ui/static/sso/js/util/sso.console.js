729b937d26653ed3753ae34ab2bdce6a06ede0c8
ben-xy <xy200362@163.com>
2021-01-26 19:47:13 +0800

    feat: address encryption and private key adjust to adapt to public mode

--- ui/static/sso/js/util/sso.console.js
+++ ui/static/sso/js/util/sso.console.js
@@ -1,11 +1,10 @@
 /******************************************************************************
- * Copyright © 2017 sharder.org.                             *
- * Copyright © 2014-2017 ichaoj.com.                                     *
+ * Copyright © 2017 sharder.org.                                              *
  *                                                                            *
  * See the LICENSE.txt file at the top-level directory of this distribution   *
  * for licensing information.                                                 *
  *                                                                            *
- * Unless otherwise agreed in a custom licensing agreement with ichaoj.com,*
+ * Unless otherwise agreed in a custom licensing agreement with sharder.org,  *
  * no part of the COS software, including this file, may be copied, modified, *
  * propagated, or distributed except according to the terms contained in the  *
  * LICENSE.txt file.                                                          *
@@ -15,7 +14,7 @@
  ******************************************************************************/
 
 /**
- * @depends {nrs.js}
+ * @depends {sso.js}
  */
 var NRS = (function (NRS, $) {
     var level = 1;
