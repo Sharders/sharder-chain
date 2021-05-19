888e37801bde0f4f269d501a0fbc70cf3882ba7a
ben-xy <xy200362@163.com>
2021-05-19 11:07:44 +0800

    feat: adapt explorer format json response

--- src/main/java/org/conch/mint/Generator.java
+++ src/main/java/org/conch/mint/Generator.java
@@ -33,6 +33,7 @@ import org.conch.consensus.poc.PocCalculator;
 import org.conch.consensus.poc.PocScore;
 import org.conch.crypto.Crypto;
 import org.conch.db.Db;
+import org.conch.db.DbUtils;
 import org.conch.env.RuntimeEnvironment;
 import org.conch.http.ForceConverge;
 import org.conch.mint.pool.SharderPoolProcessor;
@@ -45,6 +46,11 @@ import org.json.simple.JSONObject;
 
 import java.math.BigInteger;
 import java.security.MessageDigest;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.DecimalFormat;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
@@ -481,7 +487,7 @@ public class Generator implements Comparable<Generator> {
         && height > Constants.LAST_KNOWN_BLOCK) {
             if(Logger.printNow(Logger.Generator_startMining)
                 || Logger.isLevel(Logger.Level.DEBUG)) {
-                Logger.logWarningMessage("Invalid miner %s can't start auto mining or mint block. Because the holding limit of the mining is %d and current balance is %d",
+                Logger.logWarningMessage("Invalid miner %s can't start auto mining or mint block. Because the "+ Conch.COIN_UNIT +" holding limit of the mining is %d and current balance is %d",
                         minerRs,
                         (Constants.MINING_HOLDING_LIMIT / Constants.ONE_SS),
                         (accountBalanceNQT / Constants.ONE_SS));
@@ -823,6 +829,27 @@ public class Generator implements Comparable<Generator> {
         return json;
     }
     
+    public JSONObject toBlockExplorerJson() {
+        int elapsedTime = Conch.getEpochTime() - Conch.getBlockchain().getLastBlock().getTimestamp();
+        JSONObject json = new JSONObject();
+        json.put("account", Long.toUnsignedString(accountId));
+        json.put("accountRS", StringUtils.isNotEmpty(rsAddress) ? rsAddress : Account.rsAccount(accountId));
+        json.put("effectiveBalanceSS",  effectiveBalance);
+        json.put("pocScore", pocScore);
+        json.put("detailedPocScore", detailedPocScore);
+        json.put("deadline", deadline);
+        json.put("hitTime", hitTime);
+        json.put("remaining", Math.max(deadline - elapsedTime, 0));
+        CertifiedPeer boundedPeer = Conch.getPocProcessor().getBoundedPeer(accountId, Conch.getHeight());
+        Peer.Type type = (boundedPeer != null) ? boundedPeer.getType() : Peer.Type.NORMAL;
+        json.put("bindPeerType", type.getName());
+        json.put("totalBlockCount", Conch.getHeight());
+        int amount = BlockDb.getAmountByGenerator(accountId);
+        json.put("minerBlockCount", amount);
+        json.put("percentage", new DecimalFormat("0.00").format((float) (amount*100)/Conch.getHeight()).toString() + "%");
+        return json;
+    }
+
     public static void updatePocScore(PocScore pocScore){
         if(sortedMiners != null) {
             synchronized (sortedMiners) {
@@ -1052,6 +1079,32 @@ public class Generator implements Comparable<Generator> {
                 //Logger.logDebugMessage("\n\rTotal Generator detail is \n\r" + generatorDetailStr);
                 //Logger.logDebugMessage("\n\rTotal Bad Hardware Score detail is \n\r" + badHardwareScoreStr);
             }
+
+            if(lastBlock.getHeight() >= Constants.MINER_REMOVE_HIGHT){
+                for(Iterator<ActiveGenerator> it = generatorList.iterator(); it.hasNext();){
+                    ActiveGenerator activeGenerator = it.next();
+                    boolean isInTx = Db.db.isInTransaction();
+                    Connection con = null;
+                    try {
+                        con = Db.db.getConnection();
+                        PreparedStatement pstmt = con.prepareStatement("SELECT * FROM certified_peer WHERE account_id =? and delete_height != 0 and delete_height <= ?;");
+                        pstmt.setString(1,activeGenerator.getAccountId()+"");
+                        pstmt.setInt(2,lastBlock.getHeight());
+                        ResultSet rs = pstmt.executeQuery();
+                        while (rs.next()) {
+                            it.remove();
+                        }
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e.toString(), e);
+                    }finally {
+                        if (!isInTx) {
+                            DbUtils.close(con);
+                        }
+                    }
+                }
+            }
+
+
         }
         return generatorList;
     }
@@ -1128,7 +1181,7 @@ public class Generator implements Comparable<Generator> {
     }
 
     // Hub auto mining setting
-    private static final String HUB_BIND_ADDRESS = Conch.getStringProperty("sharder.HubBindAddress");
+    public static final String HUB_BIND_ADDRESS = Conch.getStringProperty("sharder.HubBindAddress");
     public static final Boolean HUB_IS_BIND = Conch.getBooleanProperty("sharder.HubBind");
     private static final String HUB_BIND_PR = Conch.getStringProperty("sharder.HubBindPassPhrase", "", true).trim();
     private static final String AUTO_MINT_ADDRESS = autoMintAccountRs();
@@ -1264,4 +1317,5 @@ public class Generator implements Comparable<Generator> {
     public static void main(String[] args) {
         System.out.println(Account.rsAccount(2792673654720227339L));
     }
+
 }
